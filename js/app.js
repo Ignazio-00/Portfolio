afff5a86433bd15de6ed56ecd605462d9b3b38ce
Ignazio Balistreri <igi-b@MacBookAirIgi.fritz.box>
2022-04-15 16:40:49 +0200

    Added Hamburger Menu

--- js/app.js
+++ js/app.js
@@ -1,25 +1,8 @@
-// Nav hamburger menu selections
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
-
 // Scroll to top selection
 const scrollUp = document.querySelector("#scroll-up");
 
 // Select nav links
 const navLink = document.querySelectorAll(".nav-link");
-
-// Hamburger menu function
-burger.addEventListener("click", () => {
-    ul.classList.toggle("show");
-});
-
-// Close Hamburger menu when a link is clicked
-navLink.forEach((link) =>
-    link.addEventListener("click", () => {
-        ul.classList.remove("show");
-    })
-);
 
 // Scroll to top functionality
 scrollUp.addEventListener("click", () => {
@@ -29,3 +12,15 @@ scrollUp.addEventListener("click", () => {
         behavior: "smooth",
     });
 });
+
+// Hamburger Menu
+const btn = document.getElementById("menu-btn")
+const nav = document.getElementById("menu")
+
+function navToggle() {
+    btn.classList.toggle("open")
+    nav.classList.toggle("hidden")
+    document.body.classList.toggle("no-scroll")
+}
+
+btn.addEventListener("click", navToggle)
