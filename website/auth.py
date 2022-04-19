bd029cd5bd9b1588c7d54eb55f063f37a5bb57f8
Ignazio Balistreri <igi-b@MacBookAirIgi.local>
2022-04-19 12:12:30 +0200

    Getting Form Data, SQLAlchemy Database Setup, Creating User Model

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,32 @@
-from flask import Blueprint, render_template, redirect, url_for
+from crypt import methods
+from unicodedata import name
+from wsgiref.simple_server import WSGIRequestHandler
+from flask import Blueprint, render_template, redirect, url_for, request
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
 
-@auth.route("/signup")
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
     return render_template("signup.html")
+
+
+@auth.route("/home", methods=["GET", "POST"])
+def home():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
+    print(email)
+    print(username)
+    print(password1)
+    print(password2)
+    return render_template("home.html")
 
 
 @auth.route("/logout")
---
+++ website/models.py
@@ -0,0 +1,14 @@
+from email.policy import default
+from enum import unique
+from sqlalchemy import PrimaryKeyConstraint
+from . import db 
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
+    date_created = db.Column(db.Date(timezone=True), default=func.now())
+    
