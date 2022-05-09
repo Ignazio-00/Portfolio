830ee7d736d97f4144e04cb6f348c41f576348c5
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2022-05-09 21:07:43 +0200

    add new password limit

--- website/models.py
+++ website/models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
     username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(80))
+    password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship("Post", backref="user", passive_deletes=True)
     comments = db.relationship("Comment", backref="user", passive_deletes=True)
