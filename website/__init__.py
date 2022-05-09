e6dcc9d6964d8bbc3c97c22c4700121daee50904
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2022-05-09 20:19:52 +0200

    add getenv ✅

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from os import path, getenv
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL') or f"sqlite:///{DB_NAME}"
     db.init_app(app)
 
     from .views import views
